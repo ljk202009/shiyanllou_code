c8681936c1faaecd18ebc1596cb4121e9e709fab
ljk202009 <1071699510@qq.com>
2022-03-17 17:03:22 +0800

    can I ?

---
+++ Jump7.java
@@ -0,0 +1,9 @@
+public class Jump7{
+	public static void main(String[] args){
+		for(int i = 1; i < 101; i++){
+			if(i % 7 != 0 && i % 10 != 7 && i / 10 != 7   ){
+				System.out.println(i);
+			}
+		}
+	}
+}
